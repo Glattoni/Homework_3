43ae83ac8182676543947a61d9d66519ebeb6d32
Glattoni <glattoni8@gmail.com>
2020-12-15 16:24:05 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -14,29 +14,30 @@ btn.addEventListener('click', function () {
 	result.innerText = `Your age is: ${age}`;
 	let chunks = age.split('');
 	let i = parseInt(chunks[chunks.length - 1]);
-	switch (i) {
-		case 0:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-		case 9:
-			result.innerText += ' лет';
-			break;
-		case 2:
-		case 3:
-		case 4:
-			result.innerText += ' года';
-			break;
-		case 1:
-			result.innerText += ' год';
-	}
-	if (chunks.length > 2) {
+	if (age > 10 && age < 20) {
+		result.innerText += ` лет`;
+	} else if (chunks.length > 2) {
 		let j = chunks[chunks.length - 2] + i;
-		if (j == 11 || 12 || 13 || 14) {
-			console.log(true);
-			result.innerText = `Your age is: ${age}`;
-			result.innerText += ' лет';
+		if (j == 11 || 12 || 13 || 14 || 15 || 16 || 17 || 18 || 19) {
+			result.innerText += ` лет`;
+		}
+	} else {
+		switch (i) {
+			case 0:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+				result.innerText += ` лет`;
+				break;
+			case 2:
+			case 3:
+			case 4:
+				result.innerText += ` года`;
+				break;
+			case 1:
+				result.innerText += ` год`;
 		}
 	}
 });
