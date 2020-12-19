cc0af7cc2eed936074693b6c716aa5fd02dbf7db
Glattoni <glattoni8@gmail.com>
2020-12-19 21:44:24 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,41 +3,30 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
 const result = document.querySelector('.result__content');
 
-// Preventing form's defalt behaviour
+// Preventing form's default behaviour
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 });
 
+
+// Checking the age value
+function ageChecker(){
+	let age = input.value;
+	result.textContent = `Your age is: ${age}`;
+	if (age%100 > 10 && age%100 < 20){
+		result.textContent += ` лет`;
+	}else if(age%10 > 1 && age%10 < 5){
+		result.textContent += ` года`;
+	}
+	else if(age%10 == 1){
+		result.textContent += ` год`;
+	}
+	else{
+		result.textContent += ` лет`;
+	}
+}
+
 // Displaying the result
 btn.addEventListener('click', function () {
-	let age = input.value;
-	result.innerText = `Your age is: ${age}`;
-	let chunks = age.split('');
-	let i = parseInt(chunks[chunks.length - 1]);
-	if (age > 10 && age < 20) {
-		result.innerText += ` лет`;
-	} else if (chunks.length > 2) {
-		let j = chunks[chunks.length - 2] + i;
-		if (j == 11 || 12 || 13 || 14 || 15 || 16 || 17 || 18 || 19) {
-			result.innerText += ` лет`;
-		}
-	} else {
-		switch (i) {
-			case 0:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			case 9:
-				result.innerText += ` лет`;
-				break;
-			case 2:
-			case 3:
-			case 4:
-				result.innerText += ` года`;
-				break;
-			case 1:
-				result.innerText += ` год`;
-		}
-	}
+	ageChecker();
 });
